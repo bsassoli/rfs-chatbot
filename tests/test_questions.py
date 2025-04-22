f9fb36fd465562c9b8415f95a6357709801649a4
Bernardino Sassoli <sassoli@gmail.com>
2025-04-22 21:37:42 +0200

    add test_app.py, modify tests/test_questions.py

--- tests/test_questions.py
+++ tests/test_questions.py
@@ -1,8 +1,9 @@
 TEST_QUESTIONS = [
     "Who was Keeling and what is his relevance?",
-    "What is the observer-expectancy effect?"
+    "What is the observer-expectancy effect?",
     "What is the role of thought experiments in scientific reasoning?",
-    "What was the Reber plan?What is a meta-analysis?",
+    "What was the Reber plan?",
+    "What is a meta-analysis?",
     "What are the pitfalls of meta-analyses?",
     "What are Lagrange points?",
 ]
